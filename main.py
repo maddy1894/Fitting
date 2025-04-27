ee8376c25d2bafc3f923cbabbdcf72f721eca7bd
unknown <madhusudhan1894@gmail.com>
2025-04-28 01:04:20 +0200

    minor change on First commit

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ xdata = data[:,1]
 ydata = data[:,0]
 def sigmoid(x, L ,x0, k, b):
     y = L / (1 + np.exp(-k*(x-x0))) + b
-    print(k)
     return (y)
 
 p0 = [max(ydata), np.median(xdata),1,min(ydata)] # this is an mandatory initial guess
